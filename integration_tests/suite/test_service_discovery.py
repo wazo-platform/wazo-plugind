b1719b796474ca235b466d7bbb8c0e89ec79e71b
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-04-04 15:11:16 -0400

    service disco: add a test with consul

--- integration_tests/suite/test_service_discovery.py
+++ integration_tests/suite/test_service_discovery.py
@@ -9,14 +9,16 @@ from xivo_test_helpers.asset_launching_test_case import AssetLaunchingTestCase
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-ASSET_ROOT = os.path.join(os.path.dirname(__file__), '..', 'assets')
 
+class BaseIntegrationTest(AssetLaunchingTestCase):
 
-class TestServiceDiscovery(AssetLaunchingTestCase):
-
-    assets_root = ASSET_ROOT
-    asset = 'plugind_only'
+    assets_root = os.path.join(os.path.dirname(__file__), '..', 'assets')
     service = 'plugind'
+
+
+class TestServiceDiscoveryNoConsulNoRabbitmq(BaseIntegrationTest):
+
+    asset = 'plugind_only'
 
     def test_that_plugind_starts_without_consul_and_rabbitmq(self):
         port = self.service_port(9503)
@@ -25,3 +27,21 @@ class TestServiceDiscovery(AssetLaunchingTestCase):
         response = requests.get(url, verify=False)
 
         assert_that(response.status_code, equal_to(200))
+
+
+class TestServiceDiscoveryWithConsul(BaseIntegrationTest):
+
+    asset = 'service_discovery'
+
+    def test_that_plugind_starts_without_consul(self):
+        port = self.service_port(8500, service_name='consul')
+        url = 'http://localhost:{}/v1/agent/services'.format(port)
+
+        response = requests.get(url, verify=False)
+
+        services = response.json()
+        for service in services.values():
+            if service['Service'] == 'wazo-plugind':
+                break
+        else:
+            self.fail('wazo-plugind is not registered: {}'.format(services))
