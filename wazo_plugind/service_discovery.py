68685e77eabd8190dd36b36dc46f390d455f2e0b
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-04-04 10:21:57 -0400

    service discovery: add a timeout on the self check

--- wazo_plugind/service_discovery.py
+++ wazo_plugind/service_discovery.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 def self_check(port):
-    conn = client.HTTPSConnection('localhost', port)
+    conn = client.HTTPSConnection('localhost', port, timeout=1)
     try:
         conn.request('GET', '/0.1/config')
     except (client.HTTPException, ConnectionRefusedError):
