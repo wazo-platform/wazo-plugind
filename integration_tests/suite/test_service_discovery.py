c46a56cdf29931033793e62fda47a751a5a94800
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-04-10 10:52:34 -0400

    integration tests: add a service disco test with rabbitmq

--- integration_tests/suite/test_service_discovery.py
+++ integration_tests/suite/test_service_discovery.py
@@ -2,10 +2,12 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 import os
+import time
 from hamcrest import assert_that, equal_to
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from xivo_test_helpers.asset_launching_test_case import AssetLaunchingTestCase
+from xivo_test_helpers.bus import BusClient
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
@@ -29,11 +31,11 @@ class TestServiceDiscoveryNoConsulNoRabbitmq(BaseIntegrationTest):
         assert_that(response.status_code, equal_to(200))
 
 
-class TestServiceDiscoveryWithConsul(BaseIntegrationTest):
+class TestServiceDiscoveryWithConsulAndRabbitMQ(BaseIntegrationTest):
 
     asset = 'service_discovery'
 
-    def test_that_plugind_starts_without_consul(self):
+    def test_that_plugind_registers_itself_on_consul(self):
         port = self.service_port(8500, service_name='consul')
         url = 'http://localhost:{}/v1/agent/services'.format(port)
 
@@ -45,3 +47,34 @@ class TestServiceDiscoveryWithConsul(BaseIntegrationTest):
                 break
         else:
             self.fail('wazo-plugind is not registered: {}'.format(services))
+
+    def test_that_plugind_sends_a_bus_message_when_started(self):
+        self.stop_service()
+        port = self.service_port(5672, service_name='rabbitmq')
+        bus_url = 'amqp://{username}:{password}@{host}:{port}//'.format(username='guest',
+                                                                        password='guest',
+                                                                        host='localhost',
+                                                                        port=port)
+        msg_accumulator = BusClient(bus_url).accumulator('service.#')
+        msg_accumulator.start_accumulating()
+
+        self.start_service()
+
+        received = self._wait_for_service_started_event(msg_accumulator)
+        msg_accumulator.stop_accumulating()
+        assert_that(received, equal_to(True), 'The bus message should have been received')
+
+    def _wait_for_service_started_event(self, msg_accumulator):
+        for _ in range(10):
+            events = msg_accumulator.get_events()
+            for event in events:
+                if self._is_service_started_event(event):
+                    return True
+            time.sleep(0.25)
+        return False
+
+    @staticmethod
+    def _is_service_started_event(event):
+        event_name, service_name = event['name'], event['data']['service_name']
+        expected = 'service_registered_event', 'wazo-plugind'
+        return event_name, service_name == expected
