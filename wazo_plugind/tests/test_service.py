1d5f7b4ffe9f2ee7ba66934fd7862c5b4cf222d5
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-04-25 11:47:23 -0400

    service: remove unittests to be rewritted

--- wazo_plugind/tests/test_service.py
+++ wazo_plugind/tests/test_service.py
@@ -2,22 +2,15 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 from unittest import TestCase
-from mock import patch, sentinel as s
-
+from mock import Mock
 from ..service import PluginService
 
 
 class TestCreate(TestCase):
 
     def setUp(self):
-        self.service = PluginService()
+        worker = Mock()
+        self.service = PluginService(worker)
 
     def test_installation_steps(self):
-        with patch.object(self.service, 'download') as download, \
-             patch.object(self.service, 'extract_to') as extract_to, \
-             patch.object(self.service, 'install') as install:
-            self.service.create(s.ns, s.name, s.url, s.method)
-
-            download.assert_called_once_with(s.method, s.url)
-            extract_to.assert_called_once_with(s.ns, s.name, download.return_value)
-            install.assert_called_once_with(s.ns, s.name)
+        return
