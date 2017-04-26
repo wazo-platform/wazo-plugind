39b13a7ce9a3fd2b4ba322d1789b304abf99bfe9
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-04-26 15:13:33 -0400

    tests: test in invalid token on install

--- integration_tests/suite/test_plugin_installation.py
+++ integration_tests/suite/test_plugin_installation.py
@@ -2,8 +2,10 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 import os
+from hamcrest import assert_that, calling, equal_to, has_entries, has_property
+from requests import HTTPError
 from wazo_plugind_client import Client
-from hamcrest import assert_that, equal_to, has_entries
+from xivo_test_helpers.hamcrest.raises import raises
 from .test_api import BaseIntegrationTest
 
 VALID_TOKEN = 'valid-token'
@@ -29,9 +31,13 @@ class TestPluginInstallation(BaseIntegrationTest):
         assert_that(install_success_exists, 'install_success was not created')
         assert_that(install_failed_exists, equal_to(False), 'install_failed should not exists')
 
-    def install_plugin(self, url, method):
+    def test_that_an_unauthorized_token_return_401(self):
+        assert_that(calling(self.install_plugin).with_args(url='/tmp/repo', method='git', token='expired'),
+                    raises(HTTPError).matching(has_property('response', has_property('status_code', 401))))
+
+    def install_plugin(self, url, method, token=VALID_TOKEN):
         port = self.service_port(9503)
-        client = Client('localhost', port=port, token=VALID_TOKEN, verify_certificate=False)
+        client = Client('localhost', port=port, token=token, verify_certificate=False)
         return client.plugins.install(url, method)
 
     def exists_in_asset(self, path):
