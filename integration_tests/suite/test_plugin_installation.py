8d6d1415645978146ed852a83be8df8d91eb2677
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-05-09 12:58:53 -0400

    tests: avoid leaving files on the filesystem after tests

--- integration_tests/suite/test_plugin_installation.py
+++ integration_tests/suite/test_plugin_installation.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 import os
+import shutil
 import uuid
 from hamcrest import (
     assert_that, calling, contains_inanyorder, equal_to, empty, has_entries, has_property, not_
@@ -57,7 +58,10 @@ class TestPluginInstallation(BaseIntegrationTest):
     asset = 'plugind_only'
 
     def setUp(self):
-        self.remove_from_asset('/results')
+        self.remove_from_asset('results')
+
+    def tearDown(self):
+        self.remove_from_asset('results')
 
     def test_when_it_works(self):
         result = self.install_plugin(url='/data/git/repo', method='git')
@@ -114,9 +118,10 @@ class TestPluginInstallation(BaseIntegrationTest):
     def remove_from_asset(self, path):
         complete_path = self.path_in_asset(path)
         try:
+            shutil.rmtree(complete_path)
             os.unlink(complete_path)
         except OSError:
             return
 
     def path_in_asset(self, path):
-        return os.path.join(self.assets_root, self.asset, path)
+        return os.path.sep.join([self.assets_root, self.asset, path])
