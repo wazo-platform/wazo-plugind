28c11995a1cd0ef3732dce3dda6ab6b022e5f432
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-05-26 09:44:00 -0400

    publisher: add error publishing

--- wazo_plugind/tests/test_bus.py
+++ wazo_plugind/tests/test_bus.py
@@ -25,11 +25,31 @@ class TestStatusPublisher(TestCase):
 
         self.publisher.publish.assert_called_once_with(expected_event)
 
+    def test_that_install_error_publish_an_error(self):
+        ctx = Mock(uuid=s.uuid)
+
+        self.status_publisher.install_error(ctx, s.error_id, s.message)
+
+        errors = {'error_id': s.error_id, 'message': s.message, 'resource': 'plugins', 'details': {}}
+        expected_event = PluginInstallProgressEvent(s.uuid, 'error', errors=errors)
+
+        self.publisher.publish.assert_called_once_with(expected_event)
+
     def test_that_uninstall_publishes_the_right_event(self):
         ctx = Mock(uuid=s.uuid)
 
         self.status_publisher.uninstall(ctx, s.status)
 
         expected_event = PluginUninstallProgressEvent(s.uuid, s.status)
+
+        self.publisher.publish.assert_called_once_with(expected_event)
+
+    def test_that_uninstall_error_publish_an_error(self):
+        ctx = Mock(uuid=s.uuid)
+
+        self.status_publisher.uninstall_error(ctx, s.error_id, s.message)
+
+        errors = {'error_id': s.error_id, 'message': s.message, 'resource': 'plugins', 'details': {}}
+        expected_event = PluginUninstallProgressEvent(s.uuid, 'error', errors=errors)
 
         self.publisher.publish.assert_called_once_with(expected_event)
