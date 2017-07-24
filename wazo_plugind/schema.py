36d3da0d31010b42aee91daf772f790c28dfde58
Sebastien Duthil <sduthil@wazo.community>
2017-07-24 16:22:18 -0400

    extract custom marshmallow validators to lib-python

--- wazo_plugind/schema.py
+++ wazo_plugind/schema.py
@@ -1,84 +1,15 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-from marshmallow import fields, pre_load, Schema, validate, validates_schema, ValidationError
+from marshmallow import pre_load, Schema, validates_schema, ValidationError
+from xivo.mallow import fields
+from xivo.mallow.validate import OneOf
+from xivo.mallow.validate import Length
+from xivo.mallow.validate import Range
+from xivo.mallow.validate import Regexp
 from .config import _MAX_PLUGIN_FORMAT_VERSION
 
 _DEFAULT_PLUGIN_FORMAT_VERSION = 0
-
-
-fields.String.default_error_messages = {
-    'required': {'message': 'Missing data for required field.',
-                 'constraint_id': 'required',
-                 'constraint': 'required'},
-    'invalid': {'message': 'Not a valid string.',
-                'constraint_id': 'type',
-                'constraint': 'string'},
-    'null': {'message': 'Field may not be null.',
-             'constraint_id': 'not_null',
-             'constraint': 'not_null'},
-}
-
-
-class Length(validate.Length):
-
-    constraint_id = 'length'
-
-    def _format_error(self, value, message):
-        msg = super()._format_error(value, message)
-
-        return {
-            'constraint_id': self.constraint_id,
-            'constraint': {'min': self.min, 'max': self.max},
-            'message': msg,
-        }
-
-
-class OneOf(validate.OneOf):
-
-    constraint_id = 'enum'
-
-    def _format_error(self, value):
-        msg = super()._format_error(value)
-
-        return {
-            'constraint_id': self.constraint_id,
-            'constraint': {'choices': self.choices},
-            'message': msg,
-        }
-
-
-class Range(validate.Range):
-
-    constraint_id = 'range'
-
-    def _format_error(self, value, *args):
-        msg = super()._format_error(value, *args)
-        constraint = {}
-        if self.min is not None:
-            constraint['min'] = self.min
-        if self.max is not None:
-            constraint['max'] = self.max
-
-        return {
-            'constraint_id': self.constraint_id,
-            'constraint': constraint,
-            'message': msg,
-        }
-
-
-class Regexp(validate.Regexp):
-
-    constraint_id = 'regex'
-
-    def _format_error(self, value):
-        msg = super()._format_error(value)
-
-        return {
-            'constraint_id': self.constraint_id,
-            'constraint': self.regex.pattern,
-            'message': msg,
-        }
 
 
 def new_plugin_metadata_schema(current_version):
