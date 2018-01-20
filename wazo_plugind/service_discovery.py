783fa02122b5c1f04ec11ade5ada41d3243e767d
Francois Blackburn <fblackburn@wazo.community>
2018-01-20 14:07:26 -0500

    service_discovery: use api version 0.2 instead 0.1
    reason: avoid warning in consol

--- wazo_plugind/service_discovery.py
+++ wazo_plugind/service_discovery.py
@@ -1,11 +1,11 @@
-# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 import requests
 
 
 def self_check(port, certificate):
-    url = 'https://localhost:{}/0.1/config'.format(port)
+    url = 'https://localhost:{}/0.2/config'.format(port)
     try:
         return requests.get(url,
                             headers={'accept': 'application/json'},
