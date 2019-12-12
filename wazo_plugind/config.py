ef8de8a7415c612a270c169745e59a38a5178f6a
Francois Blackburn <fblackburn@wazo.community>
2019-12-12 09:52:50 -0500

    update consul config to reflect default install

--- wazo_plugind/config.py
+++ wazo_plugind/config.py
@@ -68,7 +68,7 @@ _DEFAULT_CONFIG = dict(
         'exchange_type': 'topic',
     },
     consul={
-        'scheme': 'https',
+        'scheme': 'http',
         'host': 'localhost',
         'port': 8500,
         'verify': '/usr/share/xivo-certs/server.crt',
