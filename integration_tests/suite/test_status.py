b599a3fd6a7ef4e0e0992aff3ed1c5cc8d2c8819
nissou91 <achohra@wazo.io>
2022-09-23 17:18:29 -0400

    Improve status endpoint integration tests

--- integration_tests/suite/test_status.py
+++ integration_tests/suite/test_status.py
@@ -3,13 +3,10 @@
 
 from hamcrest import (
     assert_that,
-    calling,
     has_entries,
     has_entry,
 )
-from requests.exceptions import ConnectionError
 from wazo_test_helpers import until
-from wazo_test_helpers.hamcrest.raises import raises
 from .helpers.base import BaseIntegrationTest
 
 
@@ -17,40 +14,59 @@ class TestStatusAllOk(BaseIntegrationTest):
 
     asset = 'plugind_only'
 
+    def setUp(self):
+        super().setUpClass()
+
     def test_status_is_all_ok(self):
-        plugind_client = self.get_client()
-        response = plugind_client.status.get()
-        assert_that(
-            response,
-            has_entries(
-                master_tenant=has_entry('status', 'ok'),
-                rest_api=has_entry('status', 'ok'),
-                service_token=has_entry('status', 'ok'),
-            ),
-        )
+        def check_all_ok():
+            result = self.plugind.status.get()
+            assert_that(
+                result,
+                has_entries(
+                    master_tenant=has_entry('status', 'ok'),
+                    rest_api=has_entry('status', 'ok'),
+                    service_token=has_entry('status', 'ok'),
+                ),
+            )
+
+        until.assert_(check_all_ok, tries=10)
 
 
 class TestStatusWhenWazoAuthDown(BaseIntegrationTest):
 
     asset = 'plugind_only'
 
-    def test_status_fail_when_wazo_auth_is_down(self):
-        self.stop_service('plugind')
-        self.stop_service('auth')
-        self.start_service('plugind')
-        plugind_client = self.get_client()
-        assert_that(
-            calling(plugind_client.status.get),
-            raises(ConnectionError),
-        )
-        self.start_service('auth')
+    def setUp(self):
+        super().setUpClass()
 
-        response = until.return_(plugind_client.status.get, timeout=10)
+    def test_status_fail_when_wazo_auth_is_down(self):
+        self.restart_service('auth')
+        self.restart_service('plugind')
+        self.auth = self.make_mock_auth()
+        until.true(self.auth.is_up, tries=10)
+
+        self.plugind = self.make_plugind()
+        response = until.return_(self.plugind.status.get, timeout=10)
         assert_that(
             response,
             has_entries(
-                master_tenant=has_entry('status', 'ok'),
+                master_tenant=has_entry('status', 'fail'),
                 rest_api=has_entry('status', 'ok'),
-                service_token=has_entry('status', 'ok'),
+                service_token=has_entry('status', 'fail'),
             ),
         )
+
+        self.configure_wazo_auth()
+
+        def _status_all_ok():
+            result = self.plugind.status.get()
+            assert_that(
+                result,
+                has_entries(
+                    master_tenant=has_entry('status', 'ok'),
+                    rest_api=has_entry('status', 'ok'),
+                    service_token=has_entry('status', 'ok'),
+                ),
+            )
+
+        until.assert_(_status_all_ok, tries=10)
